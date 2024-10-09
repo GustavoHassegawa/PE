59fae2e0760a6903104b76dd6080e79a5ea58bde
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-10-08 21:58:13 -0300

    Update Q1.c

--- Lista1/Q1.c
+++ Lista1/Q1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int imprime_par(int a){
-    int i;
-    if (a % 2 != 0){
-        a--;
+int imprime_par(int numero){
+    if (numero % 2 != 0){
+        numero--;
     }
-    for (i = 0; i <= a; i+=2){
+    for (int i = 0; i <= numero; i+=2){
         printf("%d ", i);
     }
 }
