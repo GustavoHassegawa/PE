b8d6a87f2244965bd7c5129770eebb24ce29e437
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-11-25 22:42:15 -0300

    Update Q5.c

--- Lista5/Q5.c
+++ Lista5/Q5.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+void swap(int **a, int **b) {
+	int *temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+void imprime_vetor(int vetor[], int n) {
+	for (int i = 0; i < n; i++)
+		printf("%d, ", vetor[i]);
+	printf("\n");
+}
+
+int main() {
+	int a[3] = {5, 6, 7};
+	int b[4] = {1, 2, 0, 8};
+	
+	swap(&a, &b);
+	
+	imprime_vetor(a,4);
+	imprime_vetor(b,3);
+	return 0;
+}
