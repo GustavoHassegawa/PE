844b6764d208abd9fa021ff20f5b283a1cf219ae
gustavohassega <gustavohassega@gmail.com>
2024-12-15 00:08:21 -0300

    Lista6 update

--- Lista6/Q6.c
+++ Lista6/Q6.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int hanoi(char origem, char destino, int n, char aux) {
+    int todosMenosBase = 0, Base = 1;
+
+    if (n == 1) 
+        return 1;
+    else {
+        todosMenosBase = 2 * hanoi(origem, aux, n - 1, aux);
+        return todosMenosBase + Base;
+    }   
+}
+
+int main() {
+    printf("%d\n", hanoi('a', 'c', 3, 'b'));
+    return 0;
+}
---
+++ Lista6/Q7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int size_of_binary(int number) {
+    int tamanho = 1;
+
+    while (number > 1) {
+        number = number / 2;
+        tamanho++;
+    }
+    return tamanho;
+}
+
+void convert_to_binary(int number, char output[], int index) {
+    if (number > 1) {
+        convert_to_binary(number / 2, output, index - 1);
+    }
+    output[index - 1] = (number % 2) + '0';
+}
+
+int main() {
+    int numero, tamanho;
+    char *output;
+
+    printf("Insira o valor do numero a ser convertido: ");
+    scanf("%d", &numero);
+
+    tamanho = size_of_binary(numero);
+    output = malloc(tamanho * sizeof(int));
+
+    convert_to_binary(numero, output, tamanho);
+    printf("%s\n", output);
+
+    return 0;
+}
