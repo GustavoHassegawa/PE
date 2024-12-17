619771b812aeee7e07d688e9082a395ade455f48
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-12-16 22:16:18 -0300

    Update Q10.h

--- Lista5/Q10/Q10.h
+++ Lista5/Q10/Q10.h
@@ -15,4 +15,12 @@ void vectorint_remove(VectorInt v, int a);
 
 int vectorint_at(VectorInt v, int i);
 
+int vectorint_pertence(VectorInt v, int number);
+
+int vector_quantity(VectorInt v, int number);
+
+void vector_print(VectorInt v);
+
+void vector_destroy(VectorInt v);
+
 #endif
