713ae05d17680314e281b7ed07857deb0fbc7d7e
Gustavo Jun Miyamoto Hassegawa <90336592+GustavoHassegawa@users.noreply.github.com>
2024-12-16 22:15:58 -0300

    Update Q10.c

--- Lista5/Q10/Q10.c
+++ Lista5/Q10/Q10.c
@@ -12,7 +12,7 @@ VectorInt vectorint(void) {
 
 void vectorint_insert(VectorInt v, int a) {
     if (v->capacity == v->nelements) {
-        v->data = malloc(v->capacity * 2 * sizeof(int));
+        v->data = realloc(v->data,v->capacity * 2 * sizeof(int));
         v->capacity *= 2;
     }
     v->data[v->nelements] = a;
@@ -39,4 +39,37 @@ void vectorint_remove(VectorInt v, int a) {
 
 int vectorint_at(VectorInt v, int i) {
     return v->data[i];
+}
+
+int vectorint_pertence(VectorInt v, int number){
+	for (int i = 0; i < v->nelements; i++)
+		if (v->data[i] == number)
+			return 1;
+			
+	return 0;
+}
+
+int vector_quantity(VectorInt v, int number) {
+	int quantity = 0;
+	
+	for (int i = 0; i < v->nelements; i++)
+		if (v->data[i] == number)
+			quantity++;
+			
+	return quantity;
+}
+
+void vector_print(VectorInt v) {
+	printf("%d", v->data[0]);
+	
+	for (int i = 1; i < v->nelements; i++)
+		printf(", %d", v->data[i]);
+	
+	printf("\n");
+}
+	
+
+void vector_destroy(VectorInt v){
+	free(v->data);
+	free(v);
 }
